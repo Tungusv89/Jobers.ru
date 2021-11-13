742620040c817bde3622c94599eb7d6b21c2122d
Evgeny <tungusv89@mail.ru>
2021-11-14 01:17:25 +0500

    скрипт для pop-up

--- script.js
+++ script.js
@@ -1,32 +1,33 @@
 //перемещение меню для мобильной адаптации
-$(document).ready(function() {
-    var menu = $('.header-body').find('.header__column-menu');
+document.addEventListener('DOMContentLoaded', function() {
+    var menu = document.body.getElementsByClassName('header__column-menu');
     var w = $(window).outerWidth();
     if (w < 780) {
         $('header').append($(menu));
+        // document.getElementsByTagName('header').insertAdjacentHTML('afterend', menu);
+
     }
-});
+}, false);
 
 //выбор активного элемента в сайтбаре слева
-// if ($('.sidebar-filters__list-items').hasClass('.sidebar-filters__list-items--active')) {
-//     console.log("нашел");
-//     $('.sidebar-filters__list-items').on('click', function() {
-//         $(this).find('.circle').children().attr('src', '/img/sidebar/icons/checkcircle.svg');
-//         $(this).find('.square').children().attr('src', '/img/sidebar/icons/checksquare.svg');
-//         $(this).removeClass('sidebar-filters__list-items--active');
-//         $(this).find('.circle').next().removeClass('active');
-//         $(this).find('.square').next().removeClass('active');
-//     });
-// } else {
-//     console.log("ненашел");
-//     $('.sidebar-filters__list-items').on('click', function() {
-//         $(this).find('.circle').children().attr('src', '/img/sidebar/icons/checkedcircle.svg');
-//         $(this).find('.square').children().attr('src', '/img/sidebar/icons/checkedsquare.svg');
-//         $(this).addClass('sidebar-filters__list-items--active');
-//         $(this).find('.circle').next().addClass('active');
-//         $(this).find('.square').next().addClass('active');
-//     });
-// }
+$('.sidebar-filters__list-items').on('click', function() {
+    if (!$(this).hasClass('.sidebar-filters__list-items--active')) {
+        console.log("ненашел");
+        $(this).find('.circle').children().attr('src', '/img/sidebar/icons/checkedcircle.svg');
+        $(this).find('.square').children().attr('src', '/img/sidebar/icons/checkedsquare.svg');
+        $(this).addClass('sidebar-filters__list-items--active');
+        $(this).find('.circle').next().addClass('active');
+        $(this).find('.square').next().addClass('active');
+    } else if ($(this).hasClass('.sidebar-filters__list-items--active')) {
+        console.log("нашел");
+        $(this).find('.circle').next().removeClass('active');
+        $(this).find('.square').next().removeClass('active');
+        $(this).find('.circle').children().attr('src', '/img/sidebar/icons/checkcircle.svg');
+        $(this).find('.square').children().attr('src', '/img/sidebar/icons/checksquare.svg');
+        $(this).removeClass('sidebar-filters__list-items--active');
+
+    }
+});
 //Вызов фильтров в мобильном меню
 $('.header-menu__icon').on('click', function() {
     if (!$(this).hasClass('active')) {
